9f0bfb3b546c1e1eb3a0cd7c134e214edca2d413
LouisLao <louiselao64@gmail.com>
2021-11-07 20:48:59 -0800

    arrow function update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses/Expenses";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const App = () => {
+  <h2>Let's get started!</h2>
+  const expense = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 800,
+      date: new Date(2021, 3, 1),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return(
+    <div>
+      <h2>Let's get started</h2>
+      <Expenses items = {expense}/>
     </div>
-  );
+  )
+ 
 }
 
 export default App;
